a1658f83d70aa22e1aa79565712652428dd162f7
adhichakradhar <147488602+adhichakradhar@users.noreply.github.com>
2024-01-05 06:09:14 +0530

    #include<stdio.h>
    int main() { int r,i,j,c,a[5][5],b[5][5],s[5][5];
    
    scanf("%d %d",&r,&c);
    
    for(i=0;i<r;i++)
    { 
        for(j=0;j<c;j++)
    
    { 
        scanf("%d",&a[i][j]);
    }
    }
    scanf("%d %d",&r,&c); 
    for(i=0;i<r;i++)
    {
    
    for(j=0;j<c;j++)
    
    {
    
    scanf("%d",&b[i][j]);
    
    }
    
    }
    
    for(i=0;i<r;i++)
    
    
    {
        for(j=0;j<c;j++)
    
    { 
        s[i][j]=a[i][j]+b[i][j];
    }
    } 
    for(i=0;i<r;i++)
    {
    
    for(j=0;j<c;j++)
    
    { printf("%d ",s[i][j]);
    
    }
    printf("
    ");
    }
    
    }

--- Matrix_Addition.c
+++ Matrix_Addition.c
@@ -1,22 +1,18 @@
-#include<stdio.h> 
-int main() {
-    int r,i,j,c,a[5][5],b[5][5],s[5][5];
+#include<stdio.h>
+
+int main() { int r,i,j,c,a[5][5],b[5][5],s[5][5];
 
 scanf("%d %d",&r,&c);
 
 for(i=0;i<r;i++)
-{
+{ 
+    for(j=0;j<c;j++)
 
-for(j=0;j<c;j++)
-
-{ scanf("%d",&a[i][j]);
-
+{ 
+    scanf("%d",&a[i][j]);
 }
-
 }
-
-scanf("%d %d",&r,&c);
-
+scanf("%d %d",&r,&c); 
 for(i=0;i<r;i++)
 {
 
@@ -32,23 +28,24 @@ scanf("%d",&b[i][j]);
 
 for(i=0;i<r;i++)
 
+
 {
     for(j=0;j<c;j++)
-{ s[i][j]=a[i][j]+b[i][j];
 
+{ 
+    s[i][j]=a[i][j]+b[i][j];
 }
-
-}
-
+} 
 for(i=0;i<r;i++)
-{ for(j=0;j<c;j++) 
 {
-    printf("%d ",s[i][j]); 
-}
 
+for(j=0;j<c;j++)
+
+{ printf("%d ",s[i][j]);
+
+}
 printf("
 ");
-
 }
 
 }
